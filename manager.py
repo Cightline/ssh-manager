8f0aea284a64824e9ad6c2baac56d30f1ba6bb89
Afterburn <mujahideen.solutions@gmail.com>
2013-06-15 10:08:27 -0600

    made it save keys

--- manager.py
+++ manager.py
@@ -8,12 +8,14 @@ import yaml
 
 
 class Manager():
-    def __init__(self, ssh_pass=None):
+    def __init__(self, ssh_pass=None, overwrite=False, sudo_pass=None):
         paramiko.util.log_to_file('ssh_manager_sftp.log')
         self.client    = None
         self.transport = None
         self.config    = None
         self.ssh_pass  = ssh_pass
+        self.sudo_pass = sudo_pass
+        self.overwrite = overwrite
         self.config_path = '/home/stealth/programming/ssh_manager'
        
 
@@ -53,7 +55,6 @@ class Manager():
                     info = target.split('@')
                   
                     # get the key we need to send 
-
                     target_key = open(target_key_path, 'r') 
                     target_key_contents = target_key.read()
                     
@@ -71,11 +72,54 @@ class Manager():
                 # prompt to generate or autogen if config says so
 
 
+            if 'store_key' in self.config[user]:
+                self.get_public_key(info[0], info[1], key_type)
+                
+
+    def get_public_key(self, username, hostname, key_type):
+        if not self.client:
+            self.connect(username, hostname)
+
+        try:
+            self.sftp.stat('/home/%s/.ssh/%s' % (username, key_type))
+
+        except IOError as e:
+            # File does not exist
+            if e.errno == 2:
+                self.msg(1, 'key type: %s does not exist on host' % (key_type))
+
+
     
+        f = self.sftp.file('/home/%s/.ssh/%s' % (username, key_type), 'r')
+        pub_key = f.read()
+        f.close()
+
+        self.save_public_key(username, hostname, key_type, pub_key)
+   
+
+
+    def save_public_key(self, username, hostname, key_type, key_contents, overwrite=False):
+
+        key_path = '%s/%s/%s/%s' % (self.config_path, hostname, username, key_type)
+        key_dir  = '%s/%s/%s' % (self.config_path, hostname, username)
+
+        # If the key exists locally and we don't want to overwrite, return True
+        if os.path.exists(key_path):
+            if overwrite == False and self.overwrite != True:
+                return True
+
         
-    
+        elif not os.path.exists(key_dir):
+            os.makedirs(key_dir)
+
+        # Otherwise we should save the key 
+        with open('%s/%s/%s/%s' % (self.config_path, hostname, username, key_type), 'w') as to_write:
+            to_write.write(key_contents)
+            self.msg('saving public key from %s type %s' % (username, key_type), 0)
+            
+         
+
     def add_authorized_key(self, username, hostname, target_key):
-        # I guess if no exception is thrown, then its good ;_;
         f = self.sftp.file('/home/%s/.ssh/authorized_keys' % (username), 'a')
         f.write(target_key)
         f.close()
@@ -88,6 +132,7 @@ class Manager():
 
         try:
             self.sftp.stat('/home/%s/.ssh/authorized_keys' % (username))
+
         except IOError as e:
             # File does not exist
             if e.errno == 2:
@@ -124,12 +169,11 @@ class Manager():
             password = self.ssh_pass
         
         self.client.connect(username=username, hostname=hostname, password=password)
-
+        
 
         self.transport = self.client.get_transport()
         
         self.sftp = self.client.open_sftp()
-
 
 
         
--- ssh-manager.py
+++ ssh-manager.py
@@ -6,9 +6,14 @@ import manager
 parser = argparse.ArgumentParser(description='Options for ssh-mgr')
 
 parser.add_argument('-k', dest='ssh_pass', action='store', help='password to use when connecting')
+parser.add_argument('-g', dest='remote_target', action='store', 
+                    help='retrieve a remote public key and store it locally')
+
+parser.add_argument('-o', dest='overwrite', action='store_true', default='store_false')
 
 
 args = parser.parse_args()
 
-m = manager.Manager(args.ssh_pass)
+m = manager.Manager(args.ssh_pass, args.overwrite)
+
 m.run()
